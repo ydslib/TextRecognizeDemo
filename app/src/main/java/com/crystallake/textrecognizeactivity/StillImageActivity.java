1cbdbb7f51ae514906f608816ba22fe0d376dce7
dongsheng ye <yedongsheng@codemao.cn>
2023-02-15 12:07:46 +0800

    提交代码

---
+++ app/src/main/java/com/crystallake/textrecognizeactivity/StillImageActivity.java
@@ -0,0 +1,254 @@
+package com.crystallake.textrecognizeactivity;
+
+import static java.lang.Math.max;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.crystallake.textrecognizeactivity.textdetector.TextRecognitionProcessor;
+import com.google.mlkit.vision.text.chinese.ChineseTextRecognizerOptions;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.IOException;
+
+public class StillImageActivity extends AppCompatActivity {
+
+    private static final String TAG = "StillImageActivity";
+    private static final String TEXT_RECOGNITION_LATIN = "Text Recognition Latin";
+    private static final String TEXT_RECOGNITION_CHINESE = "Text Recognition Chinese (Beta)";
+
+    private static final int REQUEST_CHOOSE_IMAGE = 1002;
+
+    private static final String SIZE_SCREEN = "w:screen"; // Match screen width
+    private static final String SIZE_1024_768 = "w:1024"; // ~1024*768 in a normal ratio
+    private static final String SIZE_640_480 = "w:640"; // ~640*480 in a normal ratio
+    private static final String SIZE_ORIGINAL = "w:original"; // Original image size
+
+    private String selectedMode = TEXT_RECOGNITION_CHINESE;
+    private String selectedSize = SIZE_SCREEN;
+
+    private Uri imageUri;
+    private int imageMaxWidth;
+    private int imageMaxHeight;
+    private GraphicOverlay graphicOverlay;
+
+    boolean isLandScape;
+    private ImageView preview;
+
+    private VisionImageProcessor imageProcessor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_still_image);
+
+        isLandScape =
+                (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE);
+
+        preview = findViewById(R.id.preview);
+        graphicOverlay = findViewById(R.id.graphic_overlay);
+        findViewById(R.id.select_image_button)
+                .setOnClickListener(
+                        view -> {
+                            // Menu for selecting either: a) take new photo b) select from existing
+                            PopupMenu popup = new PopupMenu(StillImageActivity.this, view);
+                            popup.setOnMenuItemClickListener(
+                                    menuItem -> {
+                                        int itemId = menuItem.getItemId();
+                                        if (itemId == R.id.select_images_from_local) {
+                                            startChooseImageIntentForResult();
+                                            return true;
+                                        } else if (itemId == R.id.take_photo_using_camera) {
+//                                            startCameraIntentForResult();
+                                            return true;
+                                        }
+                                        return false;
+                                    });
+                            MenuInflater inflater = popup.getMenuInflater();
+                            inflater.inflate(R.menu.camera_button_menu, popup.getMenu());
+                            popup.show();
+                        });
+
+        View rootView = findViewById(R.id.root);
+        rootView.getViewTreeObserver()
+                .addOnGlobalLayoutListener(
+                        new ViewTreeObserver.OnGlobalLayoutListener() {
+                            @Override
+                            public void onGlobalLayout() {
+                                rootView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                                imageMaxWidth = rootView.getWidth();
+                                imageMaxHeight = rootView.getHeight() - findViewById(R.id.control).getHeight();
+                                if (SIZE_SCREEN.equals(selectedSize)) {
+                                    tryReloadAndDetectInImage();
+                                }
+                            }
+                        });
+    }
+
+    private void startChooseImageIntentForResult() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_CHOOSE_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CHOOSE_IMAGE && resultCode == RESULT_OK) {
+            // In this case, imageUri is returned by the chooser, save it.
+            imageUri = data.getData();
+            tryReloadAndDetectInImage();
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void tryReloadAndDetectInImage() {
+        Log.d(TAG, "Try reload and detect image");
+        try {
+            if (imageUri == null) {
+                return;
+            }
+
+            if (SIZE_SCREEN.equals(selectedSize) && imageMaxWidth == 0) {
+                // UI layout has not finished yet, will reload once it's ready.
+                return;
+            }
+
+            Bitmap imageBitmap = BitmapUtils.getBitmapFromContentUri(getContentResolver(), imageUri);
+            if (imageBitmap == null) {
+                return;
+            }
+
+            // Clear the overlay first
+            graphicOverlay.clear();
+
+            Bitmap resizedBitmap;
+            if (selectedSize.equals(SIZE_ORIGINAL)) {
+                resizedBitmap = imageBitmap;
+            } else {
+                // Get the dimensions of the image view
+                Pair<Integer, Integer> targetedSize = getTargetedWidthHeight();
+
+                // Determine how much to scale down the image
+                float scaleFactor =
+                        max(
+                                (float) imageBitmap.getWidth() / (float) targetedSize.first,
+                                (float) imageBitmap.getHeight() / (float) targetedSize.second);
+
+                resizedBitmap =
+                        Bitmap.createScaledBitmap(
+                                imageBitmap,
+                                (int) (imageBitmap.getWidth() / scaleFactor),
+                                (int) (imageBitmap.getHeight() / scaleFactor),
+                                true);
+            }
+
+            preview.setImageBitmap(resizedBitmap);
+
+            if (imageProcessor != null) {
+                graphicOverlay.setImageSourceInfo(
+                        resizedBitmap.getWidth(), resizedBitmap.getHeight(), /* isFlipped= */ false);
+                imageProcessor.processBitmap(resizedBitmap, graphicOverlay);
+            } else {
+                Log.e(TAG, "Null imageProcessor, please check adb logs for imageProcessor creation error");
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error retrieving saved image");
+            imageUri = null;
+        }
+    }
+
+    private Pair<Integer, Integer> getTargetedWidthHeight() {
+        int targetWidth;
+        int targetHeight;
+
+        switch (selectedSize) {
+            case SIZE_SCREEN:
+                targetWidth = imageMaxWidth;
+                targetHeight = imageMaxHeight;
+                break;
+            case SIZE_640_480:
+                targetWidth = isLandScape ? 640 : 480;
+                targetHeight = isLandScape ? 480 : 640;
+                break;
+            case SIZE_1024_768:
+                targetWidth = isLandScape ? 1024 : 768;
+                targetHeight = isLandScape ? 768 : 1024;
+                break;
+            default:
+                throw new IllegalStateException("Unknown size");
+        }
+
+        return new Pair<>(targetWidth, targetHeight);
+    }
+
+    private void createImageProcessor(){
+        if (imageProcessor != null) {
+            imageProcessor.stop();
+        }
+        try {
+            switch (selectedMode){
+                case TEXT_RECOGNITION_LATIN:
+                    if (imageProcessor != null) {
+                        imageProcessor.stop();
+                    }
+                    imageProcessor =
+                            new TextRecognitionProcessor(this, new TextRecognizerOptions.Builder().build());
+                    break;
+                case TEXT_RECOGNITION_CHINESE:
+                    if (imageProcessor != null) {
+                        imageProcessor.stop();
+                    }
+                    imageProcessor =
+                            new TextRecognitionProcessor(
+                                    this, new ChineseTextRecognizerOptions.Builder().build());
+                    break;
+            }
+        }catch (Exception e){
+            Log.e(TAG, "Can not create image processor: " + selectedMode, e);
+            Toast.makeText(
+                            getApplicationContext(),
+                            "Can not create image processor: " + e.getMessage(),
+                            Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        createImageProcessor();
+        tryReloadAndDetectInImage();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (imageProcessor != null) {
+            imageProcessor.stop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (imageProcessor != null) {
+            imageProcessor.stop();
+        }
+    }
+}
---
+++ app/src/main/java/com/crystallake/textrecognizeactivity/textdetector/TextRecognitionProcessor.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crystallake.textrecognizeactivity.textdetector;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.crystallake.textrecognizeactivity.GraphicOverlay;
+import com.crystallake.textrecognizeactivity.VisionProcessorBase;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.TextRecognizerOptionsInterface;
+
+
+/**
+ * Processor for the text detector demo.
+ */
+public class TextRecognitionProcessor extends VisionProcessorBase<Text> {
+
+    private static final String TAG = "TextRecProcessor";
+
+    private final TextRecognizer textRecognizer;
+
+    public TextRecognitionProcessor(
+            Context context, TextRecognizerOptionsInterface textRecognizerOptions) {
+        super(context);
+        textRecognizer = TextRecognition.getClient(textRecognizerOptions);
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        textRecognizer.close();
+    }
+
+    @Override
+    protected Task<Text> detectInImage(InputImage image) {
+        return textRecognizer.process(image);
+    }
+
+    @Override
+    protected void onSuccess(@NonNull Text text, @NonNull GraphicOverlay graphicOverlay) {
+        Log.d(TAG, "On-device Text detection successful");
+
+        for (Text.TextBlock textBlock : text.getTextBlocks()) {
+            System.out.println("onSuccess:" + textBlock.getText());
+        }
+
+    }
+
+    @Override
+    protected void onFailure(@NonNull Exception e) {
+        Log.w(TAG, "Text detection failed." + e);
+    }
+}
